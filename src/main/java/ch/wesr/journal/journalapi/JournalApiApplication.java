eb64346f44803815d46d1b9b64b77af48360e79d
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2022-10-09 18:16:42 +0200

    * spring boot hinzugefügt

---
+++ src/main/java/ch/wesr/journal/journalapi/JournalApiApplication.java
@@ -0,0 +1,13 @@
+package ch.wesr.journal.journalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/wesr/journal/journalapi/JournalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wesr.journal.journalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
